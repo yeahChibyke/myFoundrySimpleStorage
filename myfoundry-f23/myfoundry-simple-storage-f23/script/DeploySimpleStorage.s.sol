8cedcbd298d912c77b210914710f880cf767b2a7
chibyke <chibykepride@gmail.com>
2023-08-11 02:42:12 +0000

    Wagwan

---
+++ myfoundry-f23/myfoundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.6.0 <0.9.0;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
